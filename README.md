6835d1b496390681d821c01b973c2e0ec5ab07e2
Tyler Gould <gouldt@gmail.com>
2017-08-26 14:35:48 -0500

    basic proof of concept

---
+++ README.md
@@ -0,0 +1,10 @@
+# Eval Pal
+
+Evaluates code in many different languages.
+
+Currently depends on binaries installed on host machine.
+
+Future:
+  - Docker
+  - Demote user executing the script
+  - Authenticating requests
---
+++ app.py
@@ -0,0 +1,48 @@
+import functools
+import subprocess
+import tempfile
+
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic()
+
+
+def safely_run_subprocess(command, code):
+    with tempfile.NamedTemporaryFile() as temp:
+        temp.write(code)
+        temp.flush()
+        command = '{} {}'.format(command, temp.name)
+        completed = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    return completed
+
+@app.route("/node", methods=["POST"])
+async def evaluate_node(request):
+    safely_run_node = functools.partial(safely_run_subprocess, 'node')
+    completed_process = safely_run_node(request.body)
+    return json(completed_process.stdout)
+
+
+@app.route("/ruby", methods=["POST"])
+async def evaluate_ruby(request):
+    safely_run_ruby = functools.partial(safely_run_subprocess, 'ruby')
+    completed_process = safely_run_ruby(request.body)
+    return json(completed_process.stdout)
+
+
+@app.route("/python", methods=["POST"])
+async def evaluate_python(request):
+    safely_run_python = functools.partial(safely_run_subprocess, 'python')
+    completed_process = safely_run_python(request.body)
+    return json(completed_process.stdout)
+
+
+@app.route("/elixir", methods=["POST"])
+async def evaluate_elixir(request):
+    safely_run_elixir = functools.partial(safely_run_subprocess, 'iex')
+    completed_process = safely_run_elixir(request.body)
+    return json(completed_process.stdout)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8999)
